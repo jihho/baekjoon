662abd33f9b8ec75a6681e12be63be0c9400436d
l_jiho <dlwlgh1007@gmail.com>
2021-01-24 23:07:07 +0900

    baekjoon

--- src/baekjoon/step10/ex03.java
+++ src/baekjoon/step10/ex03.java
@@ -1,12 +1,56 @@
 package baekjoon.step10;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ex03 {
-
-	public static void main(String[] args) {
+	static char[][] arr;
+	 
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+ 
+		arr = new char[N][N];
+        
+		drawStar(0, 0, N, false);
+ 
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(arr[i][j]);
+			}
+			System.out.println();
+		}
 	}
+ 
+	static void drawStar(int x, int y, int N, boolean blank) {
 
+		if (blank) {
+			for (int i = x; i < x + N; i++) {
+				for (int j = y; j < y + N; j++) {
+					arr[i][j] = ' ';
+				}
+			}
+			return;
+		}
+ 
+
+		if (N == 1) {
+			arr[x][y] = '*';
+			return;
+		}
+ 
+		int size = N / 3;
+		int count = 0;
+		for (int i = x; i < x + N; i += size) {
+			for (int j = y; j < y + N; j += size) {
+				count++;
+				if (count == 5) { // 공백 칸일 경우
+					drawStar(i, j, size, true);
+				} else {
+					drawStar(i, j, size, false);
+				}
+			}
+		}
+	}
 }
